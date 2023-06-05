4420b8c0cbb06540b4456c93a1a7cfb23208ac48
Chris O'Brien <itpro.chrisobrien@gmail.com>
2023-06-05 11:22:07 -0400

    Update dpm-functions.ps1

--- ops-functions/dpm-functions.ps1
+++ ops-functions/dpm-functions.ps1
@@ -3,19 +3,27 @@ function add-agent {
   [CmdletBinding()]
   param (
     [Parameter(Mandatory = $true)]
-    [string]$username,
+    [string]$dbalias,
     [Parameter(Mandatory = $true)]
-    [string]$newhost,
+    [string]$pghost,
     [Parameter(Mandatory = $true)]
-    [string]$pw
+    [string]$pgpw,
+    [Parameter(Mandatory = $true)]
+    [string]$pgip
   )
   process {
-    $ts = get-date -f "MMddyyyyhhmm"
-    copy-item .\vc-agent-007.conf -Destination .\vc-agent-007.conf-$ts
+
+    [int]$incre = gci | Where-Object {$_.Name -like "vc-agent-007-backup*"}|measure|select -ExpandProperty Count
+    function comparethis([ref]$incre) {
+        $incre.value++;
+    }
+    comparethis([ref]$incre)
+    copy-item .\vc-agent-007.conf -Destination ".\vc-agent-007-backup-${incre}.conf"
+
     $c = Get-Content -Path .\vc-agent-007.conf| ConvertFrom-Json -AsHashtable
     $agentcount = $c.'drv-manual-host-uri'| Measure-Object | Select-Object -ExpandProperty Count
-    $creds = $username + "@" + $pw
-    $pg = "$newhost"+":"+"$creds"+":5432/postgres?sslenabled=true&sslmode=require"
+    $pg = "$dbalias=postgres://vividcortex%40${pghost}:${pgpw}@${pgip}:5432/postgres?sslenabled=true&sslmode=require"
+
     if($agentcount -eq 0){
       $outconfig = @{
           "drv-manual-query-capture"  = "poll"
@@ -23,15 +31,8 @@ function add-agent {
       }
       $outconfig | ConvertTo-Json -depth 100 | out-file .\vc-agent-007.conf -Force
     }
-    if($agentcount -eq 1){
-      $outconfig = @{
-          "drv-manual-query-capture"  = "poll"
-          "drv-manual-host-uri"       = $c.'drv-manual-host-uri', $pg
-      }
-      $outconfig | ConvertTo-Json -depth 100 | out-file .\vc-agent-007.conf -Force
-    }
-    if($agentcount -ge 2){
-      @($c['drv-manual-host-uri'] += $pg)
+    if($agentcount -ge 1){
+      @($c['drv-manual-host-uri'] += ",$pg")
         $outconfig = @{
           "drv-manual-query-capture"  = "poll"
           "drv-manual-host-uri"       = $c.'drv-manual-host-uri'
@@ -63,7 +64,6 @@ function remove-agent {
 }  
 # remove-agent
 ##########################
-
 function get-agent {
   $c = Get-Content -Path .\vc-agent-007.conf| ConvertFrom-Json -AsHashtable
   $uri = $c.PSObject.Properties.Value.'drv-manual-host-uri'
@@ -86,7 +86,22 @@ function get-agent {
     }
   }
 }
-# get-agent
+
+function restore-agent {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]$old
+  )
+  process {
+    Remove-Item .\vc-agent-007.conf
+    $items = gci | Where-Object {$_.Name -like "vc-agent-007-backup*"}|Select-Object -Property Name, CreationTime | sort DateTime -Descending
+    write-host $items
+    $old = Read-Host "Choose the backup number 1, 2, 3, etc"
+    Rename-Item -Path "vc-agent-007-backup-$old.conf" -NewName 'vc-agent-007.conf'
+  }
+}
+# restore-agent
 ##########################
 
 # foreach ($profileLocation in ($PROFILE | Get-Member -MemberType NoteProperty).Name)
