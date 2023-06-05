2d2374e2315285c42dbf63eb0e6a7d49fc16ceea
Chris O'Brien <itpro.chrisobrien@gmail.com>
2023-06-05 11:05:37 -0400

    Update dpm-functions.py

--- ops-functions/dpm-functions.py
+++ ops-functions/dpm-functions.py
@@ -1,82 +1,54 @@
 import json
-import os
-import glob
+import shutil
+import datetime
 
-
-def add_agent(dbalias, pghost, pgpw, pgip):
-    incre = len(glob.glob("vc-agent-007-backup*"))
-    incre += 1
-    shutil.copyfile("./vc-agent-007.conf", f"./vc-agent-007-backup-{incre}.conf")
-
+def add_agent(username, newhost, pw):
+    ts = datetime.datetime.now().strftime("%m%d%Y%H%M")
+    shutil.copyfile("./vc-agent-007.conf", f"./vc-agent-007.conf-{ts}")
     with open("./vc-agent-007.conf", "r") as file:
-        c = json.load(file)
+        content = json.load(file)
 
-    agentcount = len(c["drv-manual-host-uri"])
+    agentcount = len(content["drv-manual-host-uri"])
+    creds = f"{username}@{pw}"
+    pg = f"{newhost}:{creds}:5432/postgres?sslenabled=true&sslmode=require"
 
-    pg = f"{dbalias}=postgres://vividcortex%40{pghost}:{pgpw}@{pgip}:5432/postgres?sslenabled=true&sslmode=require"
+    if agentcount == 1:
+        content["drv-manual-query-capture"] = "poll"
+        content["drv-manual-host-uri"].append(pg)
+    elif agentcount >= 2:
+        content["drv-manual-host-uri"].append(pg)
 
-    if agentcount == 0:
-        outconfig = {
-            "drv-manual-query-capture": "poll",
-            "drv-manual-host-uri": pg
-        }
-        with open("./vc-agent-007.conf", "w") as file:
-            json.dump(outconfig, file, indent=4)
-    elif agentcount >= 1:
-        c["drv-manual-host-uri"].append(pg)
-        outconfig = {
-            "drv-manual-query-capture": "poll",
-            "drv-manual-host-uri": c["drv-manual-host-uri"]
-        }
-        with open("./vc-agent-007.conf", "w") as file:
-            json.dump(outconfig, file, indent=4)
-
+    with open("./vc-agent-007.conf", "w") as file:
+        json.dump(content, file, indent=4)
 
 def remove_agent(hostname):
     with open("./vc-agent-007.conf", "r") as file:
-        c = json.load(file)
+        content = json.load(file)
 
-    uri = c["drv-manual-host-uri"]
+    uri = content["drv-manual-host-uri"]
     remove = [u for u in uri if not u.startswith(hostname)]
-    c["drv-manual-host-uri"] = remove
 
-    outconfig = {
-        "drv-manual-query-capture": "poll",
-        "drv-manual-host-uri": c["drv-manual-host-uri"]
-    }
+    content["drv-manual-host-uri"] = remove
 
     with open("./vc-agent-007.conf", "w") as file:
-        json.dump(outconfig, file, indent=4)
-
+        json.dump(content, file, indent=4)
 
 def get_agent():
     with open("./vc-agent-007.conf", "r") as file:
-        c = json.load(file)
+        content = json.load(file)
 
-    uri = c["drv-manual-host-uri"]
+    uri = content["drv-manual-host-uri"]
     count = len(uri)
 
     if count == 1:
-        print(f"{count} configuration in this file")
+        print("1 configuration in this file")
+        print(uri[0])
     elif count > 1:
         print(f"There are {count} configurations in this file")
-    elif count == 0:
-        print("This host is not yet configured")
+        for u in uri:
+            print(u)
 
-    for u in uri:
-        print(u)
-
-
-def restore_agent(old):
-    os.remove("./vc-agent-007.conf")
-    items = sorted(glob.glob("vc-agent-007-backup*"), key=os.path.getctime, reverse=True)
-    print(items)
-    old = input("Choose the backup number 1, 2, 3, etc: ")
-    os.rename(f"vc-agent-007-backup-{old}.conf", "vc-agent-007.conf")
-
-
-# Usage examples
-add_agent("dbalias", "pghost", "pgpw", "pgip")
-remove_agent("hostname")
-get_agent()
-restore_agent("old")
+# Example usage:
+# add_agent("username", "newhost", "password")
+# remove_agent("hostname")
+# get_agent()
